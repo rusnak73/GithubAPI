b1b54f8e0c23c56362da4f749a45dc54e29b72a8
Dmytro_Rusnak <rysuy7@gmail.com>
2018-10-17 18:47:00 +0300

    Fixing bugs

--- js/main.js
+++ js/main.js
@@ -1,46 +1,50 @@
 document.addEventListener('DOMContentLoaded', function(){
     const URL = 'data/users.json';
     let listElements = document.getElementsByClassName('dropdown-item');
-    // console.log(listElements)
-    let html = '';
+    let listValue = '';
     // getByAjax();
     
     // let currentState = document.querySelector('.dropdown-menu .active').innerHTML;
     (function(){
         for (let i = 0; i < listElements.length; i++) {
             listElements[i].addEventListener('click', function(){
-                var current = document.getElementsByClassName("active");
+                let current = document.getElementsByClassName("active");
                 current[0].className = current[0].className.replace(" active", "");
                 this.className += " active";
-                document.getElementById('navbarDropdownMenuLink').innerHTML = this.innerHTML;
-                console.log(this.innerHTML)
-                checkActiveState(navbarDropdownMenuLink.innerHTML);
+                listValue = document.getElementById('navbarDropdownMenuLink').innerHTML = this.innerHTML;
+                checkActiveState(listValue);
                 
             })
         };
+        function checkActiveState(value){
+            
+            switch(value){
+            case 'Ajax':
+                // html = '';
+                return getByAjax();
+                break;
+            case 'JQuery':
+                // html = '';
+                return getByJQuery();
+                break;
+            case 'Fetch':
+                return getByFetch();
+                break;
+            case 'Axios':
+                return getByAxios();
+                break;
+                default:
+                    getByAjax();
+                
+            }
+        }  
         
     })();
-    function checkActiveState(value){
-        switch(value){
-            case 'Ajax ':
-            return getByAjax();
-            break;
-            case 'JQuery ':
-            return getByJQuery();
-            break;
-            case 'Fetch ':
-            return getByFetch();
-            break;
-            case 'Axios ':
-            return getByAxios();
-            break;
-            // default:
-            //     getByAjax();
-            
-        }
-    }
+    
     
     function getByAjax () {
+        document.querySelector('.list-container .row').innerHTML = '';
+        let html = '';
         let request = new XMLHttpRequest();
         request.open("GET", URL, true);
         request.send();
@@ -49,31 +53,29 @@ document.addEventListener('DOMContentLoaded', function(){
             for(let i = 0; i<3; i++){
                 let adminStatus = jsonResponse[i].site_admin ? "Yes" : "No";
                 html += `
-                <div class="list-container container">
-                <div class="row justify-content-between">
+                
                 <div class="user d-flex">
-                <div class="user-logo">
-                <img class="user_image" src="${jsonResponse[i].avatar_url}" alt="Image of ${jsonResponse[i].login}">
-                </div>
-                <div class="user-info">
-                <h2 class="user-name">${jsonResponse[i].login}</h2>
-                <p class="admin-value"><strong>Site Administrator:</strong> ${adminStatus}!</p>
-                </div>
+                    <div class="user-logo">
+                        <img class="user_image" src="${jsonResponse[i].avatar_url}" alt="Image of ${jsonResponse[i].login}">
+                    </div>
+                    <div class="user-info">
+                        <h2 class="user-name">${jsonResponse[i].login}</h2>
+                        <p class="admin-value"><strong>Site Administrator:</strong> ${adminStatus}!</p>
+                    </div>
                 </div>
                 <div class="user_hyperlink">
-                <a class="btn btn-primary" href="${jsonResponse[i].html_url}">Profile</a>
-                </div>
-                </div>
+                    <a class="btn btn-primary" href="${jsonResponse[i].html_url}">Profile</a>
                 </div>`;
                 // let userList = '';
                 
             }
-            document.getElementsByClassName('user-list')[0].innerHTML+=html;
+            document.querySelector('.list-container .row').innerHTML=html;
             
         }
     };
     function getByJQuery(){
-        
+        let html = '';
+        $( ".list-container .row" ).html('');
         $.ajax({
             url: URL,
             type: "GET",
@@ -100,7 +102,7 @@ document.addEventListener('DOMContentLoaded', function(){
                     // let userList = '';
                     
                 }
-                $( ".user-list" ).append(html);
+                $( ".list-container .row" ).append(html);
             },
             error: function (error) {
                 console.log(`Error${error}`);
@@ -117,6 +119,8 @@ document.addEventListener('DOMContentLoaded', function(){
         // }
     };  
     async function getByFetch(){
+        let html = '';
+        document.querySelector('.list-container .row').innerHTML = '';
         await fetch(URL)
         .then(data=>{return data.json()})
         .then(res=>{
@@ -142,7 +146,7 @@ document.addEventListener('DOMContentLoaded', function(){
                 // let userList = '';
                 
             }
-            document.getElementsByClassName('user-list')[0].innerHTML+=html;
+            document.querySelector('.list-container .row').innerHTML=html;
             
         })
     }
@@ -171,9 +175,10 @@ document.addEventListener('DOMContentLoaded', function(){
             // let userList = '';
             
         }
-        document.getElementsByClassName('user-list')[0].innerHTML+=html;
+        document.querySelector('.list-container .row').innerHTML+=html;
         
     })
     .catch(err=>console.log(err))
-}
+    }
+    getByAjax();
 });
